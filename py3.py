f5a541ca12b05c345b2eb9533df6870f47152e45
thitirat <thitirat@localhost.localdomain>
2019-06-10 13:06:29 +0700

    add file

---
+++ py3.py
@@ -0,0 +1,23 @@
+num1 = int(input("Num1 : "))
+num2 = int(input("Num2  : "))
+num3 = int(input("Num3 : "))
+num4 = int(input("Num4 : "))
+
+#if(num1>num2):
+#	if(num1>num3):
+#		print("Num1 is Greater")
+#	else:
+#		print("Num3 is Greater")
+#elif(num2>num3):
+#	print("Num2 is Greater")
+
+##########################################################
+if num1>num2 and num1>num2 and num1>num4:
+        print("Num1 is Greater")
+elif num2>num3 and num2>num4 :
+        print("Num2 is Greater")
+elif num3>num4:
+	 print("Num3 is Greater")
+else :
+	 print("Num4 is Greater")
+
